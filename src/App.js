eabbb3795467ea9bc878ccd7f53d597ba0b75eb8
Dipinder Sharma <dipinder2@gmail.com>
2021-07-13 13:50:36 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState,useEffect} from 'react'
+import axios from 'axios'
 function App() {
+  const [clicked,setClicked] = useState(false);
+  const [pokemonNames,setPokemonNames] = useState([]);
+
+
+  const handleClick = () =>{
+        setClicked(true)
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=810")
+            .then(response => {
+              return response.data
+            })
+            .then(data=>{
+              let names = data.results.map(pokemon=>pokemon.name)
+              setPokemonNames([...names]);
+            })
+            .catch(err=>{
+              console.log(err);
+          });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      clicked
+      ?<button className="btn btn-default btn-sml" disabled >Fetch Pokemons</button>
+      :<button className="btn btn-default btn-sml" onClick={handleClick}>Fetch Pokemons</button>
+    }
+    <ul className="list-group">
+    {
+      pokemonNames.map(pokemon=><li className="list-group-item">{pokemon}</li>)
+    }
+    </ul>
     </div>
   );
 }
